ec9bcca32939e6e47431fbd0acb3eb4802af8516
Palmer Clegg <palms.64@outlook.com>
2020-06-17 11:59:28 -0600

    Updated PID values to improve performance, but performance is still not acceptable. Removed bug in PID output calculation. Updated equilibirum point. Derivative control currently is not useful. The IMU is far too noisy- this noise is amplified by the derivative control. A low pass filter on the IMU is needed.

--- src/self_balancing_robot/self_balancing_robot.ino
+++ src/self_balancing_robot/self_balancing_robot.ino
@@ -2,17 +2,20 @@
 #include "DRV8835MotorShield.h"
 #include "math.h"
 
-#define GYRO_WEIGHT .95
-#define ACCEL_WEIGHT .05
-#define ZERO_ANGLE 90
+#define GYRO_WEIGHT .996
+#define ACCEL_WEIGHT .004
+#define ZERO_ANGLE 88.35
 #define PRECISION 3
 #define DEBUG true
 #define LEDPIN 13
+#define INTEGRAL_SATURATION 400
+#define S_TO_MS 1000
 
 LSM6DS3 IMU;
 DRV8835MotorShield motors;
 
-int dt = 10; //in milliseconds
+
+float dt = .005; //in seconds
 
 float gyroBiasX = 0;
 float initialAx = 0;
@@ -20,17 +23,20 @@ float thetaPrev = 0;
 float errorPrev = 0;
 float errorSum = 0;
 
-float kp = 80;
-float ki = 0;
-float kd = 35;
+float kp = 20;
+float ki = 150;
+float kd = .5;
 
 float radToDeg(float radian);
 float getAngle();
+float filterSignal(float sample);
+void updateFilterInputs(float sample);
 
 void setup() {
   Serial.begin(9600);
   IMU.begin();
   pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, HIGH);
   delay(100);
   if(DEBUG){
     Serial.println("BEGINNING CALIBRATION. DO NOT MOVE IMU");
@@ -39,15 +45,11 @@ void setup() {
   float ySum_accel = 0; 
   float zSum_accel = 0;
   int i = 0;
-  digitalWrite(LEDPIN, HIGH);
-  //if(i%100 == 0){
-  //  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
-  //}
-  for(i; i < 200; i++){
+  for(i; i < 300; i++){
     xSum_gyro += IMU.readFloatGyroX();
-    ySum_accel += IMU.readFloatAccelY(); //THE ZERO ANGLE ADJUSTMENT NEEDS TO COME HERE, NOT AFTER THE TAN2 CALCULATION
+    ySum_accel += IMU.readFloatAccelY();
     zSum_accel += IMU.readFloatAccelZ();
-    delay(dt);
+    delay(dt*S_TO_MS);
   }
   gyroBiasX = xSum_gyro/(i + 1);
   float accelAvgY = ySum_accel/(i + 1);
@@ -60,6 +62,7 @@ void setup() {
     Serial.println(gyroBiasX);
     Serial.print("Avg Angle X: ");
     Serial.println(thetaPrev);
+    Serial.println();
   }
 
   motors.flipM1(true);
@@ -71,7 +74,13 @@ void loop() {
   float theta = getAngle();
   float error = ZERO_ANGLE - theta;
   errorSum = errorSum + error;
-  float u = kp*error + kd*((error-errorPrev)/dt) + ki*errorSum;
+  if(errorSum > INTEGRAL_SATURATION){
+    errorSum = INTEGRAL_SATURATION;
+  }
+  else if(errorSum < -1*INTEGRAL_SATURATION){
+    errorSum = -1*INTEGRAL_SATURATION;
+  }
+  float u = kp*error + ki*errorSum*dt + kd*((error-errorPrev)/dt);
   if(u>400){
     u = 400;
   }
@@ -81,16 +90,22 @@ void loop() {
   
   motors.setSpeeds(u, u);
 
-  if(i%10 == 0 && DEBUG){
-    Serial.print("Theta: ");
-    Serial.println(theta, PRECISION);
-    Serial.print("u: ");
+  if(i%20 == 0 && DEBUG){
+    Serial.print("Error: ");
+    Serial.println(error, PRECISION);
+    Serial.print("P: ");
+    Serial.print(kp*error);
+    Serial.print("\tI: ");
+    Serial.print(ki*errorSum*dt);
+    Serial.print("\tD: ");
+    Serial.print(kd*((error-errorPrev)/dt));
+    Serial.print("\tu: ");
     Serial.println(u);
   }
   i++;
   thetaPrev = theta;
   errorPrev = error;
-  delay(dt);
+  delay(dt*S_TO_MS);
 }
 
 float radToDeg(float radian){
@@ -105,5 +120,5 @@ float getAngle(){
   
   float x_gyro = IMU.readFloatGyroX() - gyroBiasX; //returns angular velocity about x axis
 
-  return GYRO_WEIGHT*(thetaPrev + x_gyro*dt/1000) + ACCEL_WEIGHT*x_angle_deg;
+  return GYRO_WEIGHT*(thetaPrev + x_gyro*dt) + ACCEL_WEIGHT*x_angle_deg;
 }
